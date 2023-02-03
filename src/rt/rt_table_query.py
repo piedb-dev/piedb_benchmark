535ca42feabdecb1f63e3094b83fd717e09e94e8
dexin <dexin@qury.org>
2023-02-03 15:17:00 +0800

    rt

--- src/rt/rt_table_query.py
+++ src/rt/rt_table_query.py
@@ -7,8 +7,7 @@ root_path = os.path.abspath(__file__)
 root_path = '/'.join(root_path.split('/')[:-3])
 sys.path.append(root_path)
 from datagen.create_table_random import PreTable,InsertValue,SelectTable
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+
 
 class Table(object):
     def __init__(self):
@@ -29,8 +28,8 @@ class Table(object):
     def parse(self):
         PreTable().run()
         self.insert_0_50w()
-        time.sleep(1)
-        sql = "select count(*) from t;"
+        time.sleep(3)
+        sql = "select * from t;"
         nums = SelectTable().select(sql)
         print(nums)
 
