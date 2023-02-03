9a996ddd1b22c8498e8dbba7167a534bb9920a86
dexin <dexin@qury.org>
2023-02-03 15:54:56 +0800

    rt

--- src/rt/rt_table_query.py
+++ src/rt/rt_table_query.py
@@ -19,10 +19,12 @@ class Table(object):
         c2 = 500000
         InsertValue().run(c1,c2)
     def insert_50w_500w(self):
+        logger.info("Writing 500w data！！! please waiting....")
         c1 = 500000
         c2 = 5000000
         InsertValue().run(c1,c2)
     def insert_500w_1000w(self):
+        logger.info("Writing 1000w data！！! please waiting....")
         c1 = 5000000
         c2 = 10000000
         InsertValue().run(c1,c2)
@@ -31,10 +33,21 @@ class Table(object):
         PreTable().run()
         self.insert_0_50w()
         time.sleep(3)
-        logger.info("")
+        sql_pointget_pk = 'select * from t where id = 10000;'
+        sql_pointget_index = 'select * from t where uid = 10000;'
+        sql_pointget_normal = 'select * from t where v1 = 10000;'
 
-        sql = "select * from t;"
-        SelectTable().select(sql)
+        sql_scan_pk = 'select * from t where id < 10000 and id > 0;'
+        sql_scan_index = 'select * from t where id < 10000 and id > 0;'
+        sql_scan_normal = 'select * from t where id < 10000 and id > 0;'
+        conn = SelectTable()
+        conn.select(sql_pointget_pk)
+        conn.select(sql_pointget_index)
+        conn.select(sql_pointget_normal)
+        conn.select(sql_scan_pk)
+        conn.select(sql_scan_index)
+        conn.select(sql_scan_normal)
+
 
 if __name__ == '__main__':
     ta = Table()
