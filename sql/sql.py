3c79d1415073962eadc690757376c5807d98c9a9
dexin <dexin@qury.org>
2023-02-03 20:56:39 +0800

    view

--- sql/sql.py
+++ sql/sql.py
@@ -22,6 +22,15 @@ select_source_scan = [
     'select * from s1 where v1 < {} and v1 > {};',
 ]
 
+select_view_pointget = [
+    'select * from mv1 where id = {};',
+    'select * from mv1 where uid_max = {};',
+    'select * from mv1 where v1_max = {};',
+]
 
-
+select_view_scan = [
+    'select * from mv1 where id < {} and id > {};',
+    'select * from mv1 where uid_max < {} and uid_max > {};',
+    'select * from mv1 where v1_max < {} and v1_max > {};',
+]
 
--- src/rt/rt_materialized_view_query.py
+++ src/rt/rt_materialized_view_query.py
@@ -1,5 +1,63 @@
 # -- coding: utf-8 --
 # @Time : 2023/2/3  19:37
 # @Author : wangdexin
-# @File : rt_materialized_view_query.py 
+# @File : rt_materialized_view_query.py
+
+import psycopg2,time,datetime,random,os,sys,logging
+root_path = os.path.abspath(__file__)
+root_path = '/'.join(root_path.split('/')[:-3])
+sys.path.append(root_path)
+from datagen.create_table_random import PreTable,InsertValue,SelectTable
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+from sql.sql import select_view_scan,select_view_pointget
+
+class View(object):
+    def __init__(self):
+        pass
+
+    def parse(self):
+        ii = 0
+        while ii<5:
+            logger.info("500w data source,{} !!!!".format(ii))
+            ii+=1
+            key = random.randint(500000,5000000)
+            conn = SelectTable()
+            for sq in select_view_pointget:
+                sql = sq.format(key)
+                conn.select(sql)
+
+            key_scan = random.randint(500000,4990000)
+            for sq in select_view_scan:
+                sql = sq.format(key_scan,key_scan+10000)
+                conn.select(sql)
+
+if __name__ == '__main__':
+    ta = View()
+    ta.parse()
+
+
+
+'''
+create materialized source s1 (
+    id int PRIMARY KEY,
+    uid int,
+    v1 int,
+    v2 float,
+    s1 varchar,
+    s2 varchar,
+    time_point timestamp
+) with (
+  connector = 'kafka',
+  topic = 'test',
+  properties.bootstrap.server = 'kafka:9092',
+  scan.startup.mode = 'earliest'
+) row format json;
+
+
+create materialized view mv1 as 
+    select id,max(uid) as uid_max,max(v1) as v1_max,time_point from s1 
+group by id,time_point;
+'''
+
 
