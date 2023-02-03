f78556efd130af86b22e82d9af41007b4d23604a
dexin <dexin@qury.org>
2023-02-03 16:21:15 +0800

    sql

--- sql/sql.py
+++ sql/sql.py
@@ -6,8 +6,8 @@ select_table_pointget = [
 ]
 select_table_scan = [
     'select * from t where id < {} and id > {};',
-    'select * from t where id < {} and id > {};',
-    'select * from t where id < {} and id > {};',
+    'select * from t where uid < {} and uid > {};',
+    'select * from t where v1 < {} and v1 > {};',
 ]
 
 
